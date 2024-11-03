a08f53cb593cefae51f8097d8461ca3ade773a55
Senol Gulgonul <63923646+senolgulgonul@users.noreply.github.com>
2024-11-03 21:08:03 +0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
-from cocotb.triggers import RisingEdge
+from cocotb.triggers import RisingEdge, FallingEdge
 from cocotb.result import TestFailure
 
 @cocotb.test()
@@ -24,19 +24,21 @@ async def test_tt_um_senolgulgonul(dut):
     ]
 
     for i in range(len(expected_letters)):
-        dut.btn.value = 1
-        await RisingEdge(dut.btn)
-        await cocotb.triggers.Timer(1, units='ns')
-        dut.btn.value = 0
-        await cocotb.triggers.Timer(1, units='ns')
+        dut.ui_in.value = 1
+        await RisingEdge(dut.clk)
+        await FallingEdge(dut.clk)
+        dut.ui_in.value = 0
+        await RisingEdge(dut.clk)
+        await FallingEdge(dut.clk)
         
-        output_value = (dut.a.value << 6) | (dut.b.value << 5) | (dut.c.value << 4) | \
-                       (dut.d.value << 3) | (dut.e.value << 2) | (dut.f.value << 1) | \
-                       (dut.g.value)
+        output_value = (dut.uo_out.value << 6) | (dut.uio_out.value << 5) | (dut.uio_oe.value << 4) | \
+                       (dut.uio_oe.value << 3) | (dut.uio_oe.value << 2) | (dut.uio_oe.value << 1) | \
+                       (dut.uio_oe.value)
         dut._log.info(f'Index: {i}, Expected: {expected_letters[i]:07b}, Output: {output_value:07b}')
         
         if output_value != expected_letters[i]:
             raise TestFailure(f"Mismatch at index {i}: Expected {expected_letters[i]:07b}, got {output_value:07b}")
 
     dut._log.info("Test completed successfully.")
+
 
