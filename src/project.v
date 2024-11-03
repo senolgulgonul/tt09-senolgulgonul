a616bb949d30782f57eed0c3213fc262d587251e
Senol Gulgonul <63923646+senolgulgonul@users.noreply.github.com>
2024-11-03 23:48:43 +0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 `default_nettype none
 
 module tt_um_senolgulgonul (
-    input  wire [7:0] ui_in,    /* verilator lint_off UNUSEDSIGNAL */
+    input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
