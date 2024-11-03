b850022a5656a54cb6a1631a633c6d8481f9598b
Senol Gulgonul <63923646+senolgulgonul@users.noreply.github.com>
2024-11-03 23:39:05 +0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -51,7 +51,7 @@ module tt_um_senolgulgonul (
     assign uio_oe = 8'b11111111;
 
     // Prevent warnings for unused inputs
-    wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
+    wire _unused = &{ena, clk, rst_n, uio_in, ui_in[7:1], 1'b0};
 
 endmodule
 
